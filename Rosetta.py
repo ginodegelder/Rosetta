461358dc48bb7d9617ca8ee1b044c290d2b01a95
Yannick <yannickboucharat@yahoo.fr>
2024-07-09 17:13:01 +0200

    Corrected convertion to float hwb if not in subdict.

--- Rosetta.py
+++ Rosetta.py
@@ -246,7 +246,8 @@ dict_df_reef_free = {}
 num_key = 0
 for key, subdict in reef_params.items():
     # Convert hwb in float for forward model
-    subdict['eros__hwb'][0] = float(subdict['eros__hwb'][0])
+    if 'eros__hwb' in subdict:
+        subdict['eros__hwb'][0] = float(subdict['eros__hwb'][0])
     # Generating the dataframes from the subdicts.
     # The first subdict (num_key = 0) contains both fixed and free parameters.
     if num_key == 0: 
